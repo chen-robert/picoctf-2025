42dcc3a3304e595ae1b13e48a2a1436f3f50ec95
Robert Chen <me@robertchen.cc>
2025-02-19 23:52:01 -0500

    update testing script

--- test_circuits.sh
+++ test_circuits.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# Set the endpoint to REMOTE if defined, otherwise default to localhost
+ENDPOINT=${REMOTE:-"http://localhost:3000/check"}
+
 echo "Testing all circuits in one request..."
-curl -X POST http://localhost:3000/check \
+curl -X POST "$ENDPOINT" \
   -H "Content-Type: application/json" \
   -d '{"circuit":[
     {"input1":5,"input2":5,"output":1},
     {"input1":6,"input2":6,"output":2},
     {"input1":7,"input2":7,"output":3},
     {"input1":8,"input2":8,"output":4}
-  ]}' 
+  ]}'
